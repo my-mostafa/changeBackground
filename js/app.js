071922d83f7bb3f630da2a145063f39a3d5cccc9
mostafa <immostafaeh@gmail.com>
2024-10-25 15:36:06 +0330

    project change background

---
+++ js/app.js
@@ -0,0 +1,25 @@
+let roor = document.body
+setInterval(function(){
+    let r = Math.random()*255
+    let g = Math.random()*255
+    let b = Math.random()*255
+    roor.style.background ='rgb'+'('+r+','+g+','+b+')'
+} , 2000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
